fe65647da58a56e22ba100556130f90668c95986
mahbub-sakib <mahbub.sakib418@gmail.com>
2026-02-17 19:01:45 +0600

    getter setter

--- es6class.js
+++ es6class.js
@@ -12,9 +12,31 @@ class PersonCl {
 }
 
 const sakib = new PersonCl('Sakib', 1990);
-console.log(sakib);
+// console.log(sakib);
 sakib.calcAge();
 
 PersonCl.prototype.greet = function () {
     console.log(`Hey ${this.firstName}`);
 }
+
+
+// -----------getter and setter------------
+
+const account = {
+    owner: 'Sakib',
+    movements: [300, 150, 500, 1000, 200],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+}
+
+account.latest = 20;
+
+// console.log(account.latest)
+
+console.log(account);
