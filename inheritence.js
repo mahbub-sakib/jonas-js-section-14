71e905e28a71800ae3d03e9c691573685a1b7a9e
mahbub-sakib <mahbub.sakib418@gmail.com>
2026-02-21 19:39:13 +0600

    inheritence: using ES6

--- inheritence.js
+++ inheritence.js
@@ -22,16 +22,68 @@ Student.prototype.introduce = function () {
 
 const sakib = new Student('Sakib', 2000, 'CSE');
 
-sakib.introduce();
-sakib.calcAge();
+// sakib.introduce();
+// sakib.calcAge();
 
-console.log(sakib.__proto__);
-console.log(sakib.__proto__.__proto__);
+// console.log(sakib.__proto__);
+// console.log(sakib.__proto__.__proto__);
 
 Student.prototype.constructor = Student;
 
-console.dir(Student.prototype.constructor)
+// console.dir(Student.prototype.constructor)
 
-console.log(sakib instanceof Student);
-console.log(sakib instanceof Person);
-console.log(sakib instanceof Object);
+// console.log(sakib instanceof Student);
+// console.log(sakib instanceof Person);
+// console.log(sakib instanceof Object);
+
+
+//----------------------------ES6 class--------------------------------------
+
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+
+    greet() {
+        console.log(`Hey ${this.fullName}`);
+    }
+    get age() {
+        return 2037 - this.birthYear;
+    }
+    set fullName(name) {
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+    get fullName() {
+        return this._fullName;
+    }
+
+    static hey() {
+        console.log('Hey there');
+    }
+}
+
+class StudentCl extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        super(fullName, birthYear);
+        this.course = course;
+    }
+
+    introduce() {
+        console.log(`My name is ${this.fullName} and i study ${this.course}`);
+    }
+
+    calcAge() {
+        console.log(`I am ${2037 - this.birthYear} years old, but as a student feels like ${2037 - this.birthYear + 10}`);
+    }
+}
+
+const abony = new StudentCl('Sadia Afrin Abony', 1996, 'CSE');
+
+abony.introduce();
+abony.calcAge();
