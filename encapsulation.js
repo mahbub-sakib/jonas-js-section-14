3fd591f193e97f1457de2c442d604fb70846b517
mahbub-sakib <mahbub.sakib418@gmail.com>
2026-02-22 19:20:24 +0600

    access modifiers ()public private

--- encapsulation.js
+++ encapsulation.js
@@ -1,27 +1,37 @@
 'use strict';
 
 class Account {
+    locale = navigator.language;
+    bank = 'Bankist';
+    #movements = [];
+    #pin;
+
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movement = [];
-        this.locale = navigator.language;
+        this.#pin = pin;
+        // this.movement = [];
+        // this.locale = navigator.language;
 
         console.log(`Thanks for opening account ${this.owner}`);
     }
+
+    getMovements() {
+        return this.#movements;
+    }
+
     deposit(val) {
-        this.movement.push(val);
+        this.#movements.push(val);
     }
     withdraw(val) {
         this.deposit(-val);
     }
 
-    approveLoan(val) {
+    #approveLoan(val) {
         return true;
     }
     requestLoan(val) {
-        if (this.approveLoan(val)) {
+        if (this.#approveLoan(val)) {
             this.deposit(val);
             console.log('Loan approved');
         }
@@ -35,6 +45,7 @@ const acc1 = new Account('Sakib', 'TAKA', 1234);
 
 acc1.deposit(250);
 acc1.withdraw(140);
+acc1.movements = [];
 
 console.log(acc1);
 
